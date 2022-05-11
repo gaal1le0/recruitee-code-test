51bb0d76d330f689a85a8b3890fdb4e04e40b14f
Emanuel Martínez <me@manumartinez.es>
2022-05-11 23:25:28 +0200

    create project

---
+++ Recruitee-code-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recruitee-code-test
+//
+//  Created by Sergio Becerril on 11/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
