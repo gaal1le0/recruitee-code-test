b554d33dc1d8c75b821a32743f03d45df7f2c1e4
Emanuel Martínez <me@manumartinez.es>
2022-05-12 13:39:40 +0200

    Added new constants

--- Recruitee-code-test/Constants.swift
+++ Recruitee-code-test/Constants.swift
@@ -9,14 +9,25 @@ import Foundation
 
 struct Constants {
     
-    static let apiKey = "a039144774mshca594ff07e2c4c3p1b2b61jsn6b1c931eed8"
+    static let apiKey = "a039144774mshca594ff07e2c4c3p1b2b61jsn6b1c931eed8c"
     
     struct URL {
         static let main = "https://yh-finance.p.rapidapi.com/"
     }
     
     struct Endpoints {
+        
+        static let singleEndPoint = "yh-finance.p.rapidapi.com"
         static let urlMarketGetSummary = "market/v2/get-summary"
         static let urlStockGetSummary = "stock/v2/get-summary"
+    }
+    
+    struct Headers {
+        static let host = "X-RapidAPI-Host"
+        static let apyKey = "X-RapidAPI-Key"
+    }
+    
+    struct Method {
+        static let get = "GET"
     }
 }
