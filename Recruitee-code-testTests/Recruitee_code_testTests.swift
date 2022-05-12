8f026bada5050b60b490fb1b2696d7458448ab60
Emanuel Martínez <me@manumartinez.es>
2022-05-12 14:56:39 +0200

    Create unit test

--- Recruitee-code-testTests/Recruitee_code_testTests.swift
+++ Recruitee-code-testTests/Recruitee_code_testTests.swift
@@ -2,35 +2,39 @@
 //  Recruitee_code_testTests.swift
 //  Recruitee-code-testTests
 //
-//  Created by Sergio Becerril on 11/5/22.
+//  Created by Emanuel Martínez on 12/5/22.
 //
 
 import XCTest
 @testable import Recruitee_code_test
 
 class Recruitee_code_testTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testCallAPIWorking()
+    {
+        //WHEN
+        let conectionWrapper = ManagerConnections()
+        //GIVEN
+        conectionWrapper.getSummaryMarket().subscribe { stock in
+        //THEN
+            XCTAssertNotNil(stock.element?.first?.market)
         }
+        .dispose()
     }
-
+    
+    func testCallDetailAPIWorking()
+    {
+        //WHEN
+        let conectionWrapper = ManagerConnections()
+        let exchangeNameMocked = "CBOT"
+        
+        //GIVEN
+        conectionWrapper.getDetailStock(fullExchangeName: exchangeNameMocked)
+            .subscribe { stock in
+                
+        //THEN
+                XCTAssertEqual(stock.element?.symbol, exchangeNameMocked)
+            }
+            .dispose()
+    }
 }
